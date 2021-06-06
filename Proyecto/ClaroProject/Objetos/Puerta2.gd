39b00fae3db988e55ad22c405819507fc86f204a
German <Dartsch>
2021-06-06 16:37:25 -0300

    Agregados varios sonidos al juego

--- Proyecto/ClaroProject/Objetos/Puerta2.gd
+++ Proyecto/ClaroProject/Objetos/Puerta2.gd
@@ -5,5 +5,7 @@ extends StaticBody
 func _on_Area_body_entered(body):
 	if body.has_method("getLlaves"):
 		if body.getLlaves() > 0:
-			body.perderLlave()
-			queue_free()
+			if body.has_method("perderLlave"):
+				body.perderLlave()
+				get_node("../../Sonidos/Puerta").play()
+				queue_free()
--- Proyecto/ClaroProject/Pruebas/Spatial.gd
+++ Proyecto/ClaroProject/Pruebas/Spatial.gd
@@ -4,8 +4,14 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
 func _input(event):
+	liberarMouse(event)
+	recapturarMouse(event)
+
+func liberarMouse(event):
 	if event.is_action_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func recapturarMouse(event):
 	if event.is_action_pressed("Interactuar"):
 		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
