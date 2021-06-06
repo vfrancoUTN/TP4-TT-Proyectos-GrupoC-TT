ac786b6467cad7ba168cebab032a67e02557034c
German <Dartsch>
2021-06-06 17:23:14 -0300

    Agregado modulo de inventario y stats

---
+++ Proyecto/ClaroProject/Jugador/Stats.gd
@@ -0,0 +1,6 @@
+extends Node #ac van a ir los stats que despus van a mostarse en la pantalla de resultados
+
+var tiempo = 0
+
+func _on_Timer_timeout():
+	tiempo += 1
--- Proyecto/ClaroProject/Objetos/Puerta2.gd
+++ Proyecto/ClaroProject/Objetos/Puerta2.gd
@@ -3,8 +3,8 @@ extends StaticBody
 
 
 func _on_Area_body_entered(body):
-	if body.has_method("getLlaves"):
-		if body.getLlaves() > 0:
+	if body.has_method("chequearLlaves"):
+		if body.chequearLlaves() > 0:
 			if body.has_method("perderLlave"):
 				body.perderLlave()
 				get_node("../../Sonidos/Puerta").play()
