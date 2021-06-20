0f8d51975d65de5040a93bcde77ee627b5a4b717
Exequiel <lucas.moreno@alumnos.frm.utn.edu.ar>
2021-06-20 18:56:14 -0300

    Actualizaciones Modo Prueba

--- Proyecto/ClaroProject/Puzzles/Puzzle1.gd
+++ Proyecto/ClaroProject/Puzzles/Puzzle1.gd
@@ -1,18 +1,27 @@
 extends Control
 
-#
-#hide() desactiva el nodo
+var enPuzzle = false
+
+func getPuzzle():
+	return enPuzzle
+
+func setPuzzle(valor:bool):
+	enPuzzle = valor
+ 
 
 func _on_Opcion_1_pressed():
+	setPuzzle(false)
 	get_tree().change_scene("res://Interfaz/PantallaDerrota.tscn")
 	hide() # Replace with function body.
 
 
 func _on_Opcion_2_pressed():
+	setPuzzle(false)
 	get_tree().change_scene("res://Interfaz/PantallaDerrota.tscn")
 	hide() # Replace with function body
 
 
 func _on_Opcion_3_pressed():
+	setPuzzle(false)
 	get_tree().paused == false
 	hide() # Replace with function body.
